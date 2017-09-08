ef3045cbbd54cfbaf009a0e316bc74d24df3312c
omochi <omochi.metaru@gmail.com>
2017-09-08 17:50:48 +0900

    debugDump()

--- Sources/DebugReflect/DebugReflectable.swift
+++ Sources/DebugReflect/DebugReflectable.swift
@@ -1,15 +1,22 @@
 public protocol DebugReflectable : CustomDebugStringConvertible {
     func debugReflect() -> DebugReflectValue
     
+    func debugDump() -> String
+    
     static var debugTypePrinting: Bool { get }
 }
 
 extension DebugReflectable {
     public var debugDescription: String {
+        return debugDump()
+    }
+    
+    public func debugDump() -> String {
         let writer = DebugReflectWriter()
         writer.write(self)
         return writer.output()
     }
+    
     public static var debugTypePrinting: Bool {
         return true
     }
