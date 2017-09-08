f907aa3c54ee01c69c32347135449813943fd6e9
omochi <omochi.metaru@gmail.com>
2017-09-08 16:04:38 +0900

    rename repository name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "DebugReflectable",
+    name: "DebugReflect",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
