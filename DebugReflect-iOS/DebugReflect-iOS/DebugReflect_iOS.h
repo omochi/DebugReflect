e29d2b9873d094ed973ef7e97cbcf0306f9d3eef
omochi <omochi.metaru@gmail.com>
2017-10-10 15:21:37 +0900

    DebugReflect iOS xcodeproj

---
+++ DebugReflect-iOS/DebugReflect-iOS/DebugReflect_iOS.h
@@ -0,0 +1,18 @@
+//
+//  DebugReflect_iOS.h
+//  DebugReflect-iOS
+//
+//  Created by omochimetaru on 2017/10/10.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DebugReflect_iOS.
+FOUNDATION_EXPORT double DebugReflect_iOSVersionNumber;
+
+//! Project version string for DebugReflect_iOS.
+FOUNDATION_EXPORT const unsigned char DebugReflect_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DebugReflect_iOS/PublicHeader.h>
+
+
